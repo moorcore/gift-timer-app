359c2d66c91b5852f9f591420291833c3bd1ac2e
Maxim <deuswn@gmail.com>
2024-10-27 15:04:32 +0100

    Initial Commit

---
+++ Timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by Maxim Boykin on 27.10.24..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
